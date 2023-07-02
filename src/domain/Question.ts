4d6a81b4d3a01308676d92491f224d2cb9ba1367
LeandroSantosP <skp.tridimensional@gmail.com>
2023-07-01 22:00:43 -0300

    updated

--- src/domain/Question.ts
+++
@@ -1,7 +0,0 @@
-export class Question {
-    constructor(
-        readonly questionName: string,
-        readonly answers: { [key: string]: string },
-        readonly correctAnswer: number
-    ) {}
-}
--- src/infra/repositories/QuizRepositoryMemory.ts
+++
@@ -1,9 +0,0 @@
-import { IQuizRepository } from "@/application/interfaces/IQuizRepository";
-import { Quiz } from "@/domain/Quiz";
-
-export class QuizRepositoryMemory implements IQuizRepository {
-    quizz: Quiz[] = [];
-    async save(quiz: Quiz): Promise<void> {
-        this.quizz.push(quiz);
-    }
-}
